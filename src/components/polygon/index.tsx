05b06af1772b45c2a415b868ee6cca98d047b52f
test1883 <parth.eng1210@gmail.com>
2023-08-27 08:14:09 +0000

    updates

--- src/components/polygon/index.tsx
+++ src/components/polygon/index.tsx
@@ -237,13 +237,17 @@ const Styles = {
        padding: 8px;
        font-weight: 600;
        font-size: 15px;
-       width: 120px;
+       width: 170px;
        background-color: #FA9703;
        border-radius: 30px;
        text-align: center;
        box-shadow: rgb(0, 0, 0, 0.19) 0px 10px 20px, rgb(0, 0, 0, 0.23) 0px 6px 6px;
        &>svg {
          fill: #fff;
+       }
+       @media screen and (max-width: 600px) {
+        font-size: 12px;
+        width: 130px;
        }
      `,
    
